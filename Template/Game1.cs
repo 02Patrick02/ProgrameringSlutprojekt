44f05ef475dc97b90752a2588924765c95d00570
02patrick02 <paku02001@utb.vaxjo.se>
2020-03-27 09:48:03 +0100

    Jag lade till så att enemy spawnar random och ökar sedan succesivt med tiden.

--- Template/Game1.cs
+++ Template/Game1.cs
@@ -14,19 +14,22 @@ namespace Template
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
 
-        private Texture2D Spelare1, skott, Spelare2, background, EnemySpawn;
-        private Vector2 spelare1pos, skottpos, Spelare2pos, EnemyPos, BackgroundPos;
-        private Rectangle storlekSpelare1, storlekspelare2, StorlekEnemy;
+        private Texture2D Spelare1, skott1, skott2, Spelare2, background, EnemySpawn;
+        private Vector2 spelare1pos, Spelare2pos;
+        private Rectangle storlekSpelare1, storlekspelare2;
 
+        private Random rnd = new Random();
 
-        Random rnd = new Random();
+      
+       
+        
 
         private List<Vector2> Spelare1skottpos = new List<Vector2>();
         private List<Vector2> Spelare2skottpos = new List<Vector2>();
         private List<Vector2> RandomEnemySpawn = new List<Vector2>();
 
-
-        int screenwidth, screenheight;
+      
+        int screenwidth, screenheight, EnemySpawnPos = 0, EnemyTimer = 0, SpawnRate = 60;
         private KeyboardState kNewstate, kOldstate;
 
 
@@ -49,13 +52,19 @@ namespace Template
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            spelare1pos = new Vector2(300, 200);
-            Spelare2pos = new Vector2(100, 150);
-            BackgroundPos = new Vector2(0, 0);
-            EnemyPos = new Vector2(0, 0);
-            skottpos = new Vector2(15, 10);
+            spelare1pos = new Vector2(1200, 500);
+            Spelare2pos = new Vector2(550, 480);
+            
 
-           
+
+            graphics.PreferredBackBufferWidth = GraphicsDevice.DisplayMode.Width;
+            graphics.PreferredBackBufferHeight = GraphicsDevice.DisplayMode.Height;
+            graphics.ApplyChanges();
+
+
+
+    
+
 
 
             base.Initialize();
@@ -71,7 +80,8 @@ namespace Template
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             Spelare1 = Content.Load<Texture2D>("Spelare1");
-            skott = Content.Load<Texture2D>("skott");
+            skott1 = Content.Load<Texture2D>("skott1");
+            skott2 = Content.Load<Texture2D>("skott2");
             Spelare2 = Content.Load<Texture2D>("Spelare2");
             background = Content.Load<Texture2D>("background");
             EnemySpawn = Content.Load<Texture2D>("EnemySpawn");
@@ -107,8 +117,9 @@ namespace Template
 
 
 
-            storlekspelare2 = new Rectangle((int)Spelare2pos.X, (int)Spelare2pos.Y, 100, 85);//storlek på fienden
-            storlekSpelare1 = new Rectangle((int)spelare1pos.X, (int)spelare1pos.Y, 100, 85);//storleken på spritebatchen Spelare1 
+            storlekspelare2 = new Rectangle((int)Spelare2pos.X, (int)Spelare2pos.Y, 150, 150);//storlek på fienden
+            storlekSpelare1 = new Rectangle((int)spelare1pos.X, (int)spelare1pos.Y, 130, 130);//storleken på spritebatchen Spelare1 
+        
 
             kNewstate = Keyboard.GetState();
             KeyboardState a = Keyboard.GetState();
@@ -126,28 +137,12 @@ namespace Template
 
             if (a.IsKeyDown(Keys.Space) && kOldstate.IsKeyUp(Keys.Space))//skott för Spelare1 spriten
             {
-                Spelare1skottpos.Add(spelare1pos + skottpos);
+                Spelare1skottpos.Add(spelare1pos + new Vector2(10, 0));
             }
             for (int i = 0; i < Spelare1skottpos.Count; i++)
             {
-                Spelare1skottpos[i] = Spelare1skottpos[i] - new Vector2(0, 1);
+                Spelare1skottpos[i] = Spelare1skottpos[i] - new Vector2(0, 5);
             }
-
-
-
-
-
-            if (a.IsKeyDown(Keys.U) && kOldstate.IsKeyUp(Keys.U))// Enemy spawnar när man trycker på U
-            {
-                RandomEnemySpawn.Add(EnemyPos);
-            }
-            for (int i = 0; i < RandomEnemySpawn.Count; i++)
-            {
-                RandomEnemySpawn[i] = RandomEnemySpawn[i] - new Vector2(0, -1);
-            }
-
-
-
 
 
 
@@ -165,11 +160,11 @@ namespace Template
 
             if (a.IsKeyDown(Keys.Z) && kOldstate.IsKeyUp(Keys.Z))//skott för fienden
             {
-                Spelare2skottpos.Add(Spelare2pos + skottpos);
+                Spelare2skottpos.Add(Spelare2pos + new Vector2(89, 0));
             }
             for (int i = 0; i < Spelare2skottpos.Count; i++)
             {
-                Spelare2skottpos[i] = Spelare2skottpos[i] - new Vector2(0, 1);
+                Spelare2skottpos[i] = Spelare2skottpos[i] - new Vector2(0, 5);
             }
 
 
@@ -180,27 +175,50 @@ namespace Template
                 spelare1pos.X = 0;
             if (spelare1pos.X + storlekSpelare1.Width >= screenwidth)
                 spelare1pos.X = screenwidth - storlekSpelare1.Width;
-
             if (spelare1pos.Y <= 0) //i Y-led så att Spelare1 spriten inte kommer utanför skärmen
                 spelare1pos.Y = 0;
             if (spelare1pos.Y + storlekSpelare1.Height >= screenheight)
                 spelare1pos.Y = screenheight - storlekSpelare1.Height;
-
-
-
             if (Spelare2pos.X <= 0)//göt så att Spelare2 spriten inte kan komma utanför skärmen i X-led
                 Spelare2pos.X = 0;
             if (Spelare2pos.X + storlekspelare2.Width >= screenwidth)
                 Spelare2pos.X = screenwidth - storlekspelare2.Width;
-
             if (Spelare2pos.Y <= 0) //i Y-led så att Spelare2 spriten inte kommer utanför skärmen
                 Spelare2pos.Y = 0;
             if (Spelare2pos.Y + storlekspelare2.Height >= screenheight)
                 Spelare2pos.Y = screenheight - storlekspelare2.Height;
 
-    
 
-                    RemoveObjects();
+
+            if(SpawnRate > 15)//Om Spawnrate är över 15 så kommer den minska med 5 var 10 sekund, 10s = enemytimer 600
+            {
+                EnemyTimer++;
+                if (EnemyTimer == 120)
+                {
+                    SpawnRate -= 5;
+                    EnemyTimer = 0;
+                }
+            }
+            if(SpawnRate <= 15 && SpawnRate > 5)//Om Spawnrate är 5<x<15 så kommer den minska med 1 var 10 sekund, 10s = enemytimer 600
+            {
+                EnemyTimer++;
+                if (EnemyTimer == 120)
+                {
+                    SpawnRate -= 1;
+                    EnemyTimer = 0;
+                }
+            }
+            EnemySpawnPos = rnd.Next(0, screenwidth);
+            if (rnd.Next(0, SpawnRate) == 0)
+            {
+                RandomEnemySpawn.Add(new Vector2(EnemySpawnPos, 0));
+            }
+            for (int i = 0; i < RandomEnemySpawn.Count; i++)
+            {
+                RandomEnemySpawn[i] = RandomEnemySpawn[i] - new Vector2(0, -2);
+            }
+
+            RemoveObjects();
 
             kOldstate = kNewstate;
 
@@ -215,40 +233,42 @@ namespace Template
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            
             spriteBatch.Begin();
-            spriteBatch.Draw(background, BackgroundPos, Color.White);
+            spriteBatch.Draw(background, new Rectangle(0, 0, GraphicsDevice.DisplayMode.Width, GraphicsDevice.DisplayMode.Height), Color.White);
             spriteBatch.Draw(Spelare1, storlekSpelare1, Color.White);
             spriteBatch.Draw(Spelare2, storlekspelare2, Color.White);
-            spriteBatch.Draw(EnemySpawn, StorlekEnemy, Color.White);
+            spriteBatch.Draw(EnemySpawn, new Rectangle(0,0,0,0), Color.White);
 
 
 
 
-            foreach (Vector2 SkottPos in Spelare1skottpos) //skott för spelare1
+            foreach (Vector2 Spelare1skottpos in Spelare1skottpos) //skott för spelare1
             {
                 Rectangle rec = new Rectangle();
-                rec.Location = SkottPos.ToPoint();
-                rec.Size = new Point(20, 20);
-                spriteBatch.Draw(skott, rec, Color.White);
+                rec.Location = Spelare1skottpos.ToPoint();
+                rec.Size = new Point(30, 30);
+                spriteBatch.Draw(skott1, rec, Color.White);
             }
 
-            foreach (Vector2 SkottPos in Spelare2skottpos)//skott för spelare2
+            foreach (Vector2 Spelare2skottpos in Spelare2skottpos)//skott för spelare2
             {
                 Rectangle rec = new Rectangle();
-                rec.Location = SkottPos.ToPoint();
-                rec.Size = new Point(20, 20);
-                spriteBatch.Draw(skott, rec, Color.White);
+                rec.Location = Spelare2skottpos.ToPoint();
+                rec.Size = new Point(40, 40);
+                spriteBatch.Draw(skott2, rec, Color.White);
             }
 
 
-            foreach (Vector2 EnemyPos in RandomEnemySpawn)//skott för spelare2
+            foreach (Vector2 RandomEnemySpawn in RandomEnemySpawn)
             {
                 Rectangle rec = new Rectangle();
-                rec.Location = EnemyPos.ToPoint();
-                rec.Size = new Point(20, 20);
+                rec.Location = RandomEnemySpawn.ToPoint();
+                rec.Size = new Point(80, 80);
                 spriteBatch.Draw(EnemySpawn, rec, Color.White);
             }
+
+          
 
 
             spriteBatch.End();
@@ -272,8 +292,27 @@ namespace Template
 
             Spelare1skottpos = temp;
 
-          
+          List<Vector2> temp2 = new List<Vector2>();
+            foreach (var item in Spelare2skottpos)
+            {
+                if (item.Y >= 0)
+                {
+                    temp2.Add(item);
+                }
+            }
 
+            Spelare2skottpos = temp2;
+
+            List<Vector2> temp3 = new List<Vector2>();
+            foreach (var item in RandomEnemySpawn)
+            {
+                if (item.Y <= screenheight)
+                {
+                    temp3.Add(item);
+                }
+            }
+
+            RandomEnemySpawn = temp3;
         }
     }
 }
