bed4f8e0b17c02ad568b36ca5aaee88a2378e56f
02patrick02 <paku02001@utb.vaxjo.se>
2020-04-17 15:04:17 +0200

    Skapade en klass för Spelare1, spelare2 och fiende

--- Template/Game1.cs
+++ Template/Game1.cs
@@ -6,36 +6,17 @@ using System.Collections.Generic;
 
 namespace Template
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class Game1 : Game
     {
+   
+        Spelare s1 = new Spelare();
+        Spelare1 s2 = new Spelare1();
+        Fiende F = new Fiende();
+       
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
-
-        private Texture2D Spelare1, skott1, skott2, Spelare2, background, EnemySpawn;
-        private Vector2 spelare1pos, Spelare2pos;
-        private Rectangle storlekSpelare1, storlekspelare2;
-
-        private Random rnd = new Random();
-
-
-
-
-
-        private List<Vector2> Spelare1skottpos = new List<Vector2>();
-        private List<Vector2> Spelare2skottpos = new List<Vector2>();
-        private List<Vector2> RandomEnemySpawn = new List<Vector2>();
-
-
-        int screenwidth, screenheight, EnemySpawnPos = 0, EnemyTimer = 0, SpawnRate = 60;
-        private KeyboardState kNewstate, kOldstate;
-
-
-
-
-
+        private static bool quit;
+        private Texture2D background;
 
         public Game1()
         {
@@ -43,276 +24,71 @@ namespace Template
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            spelare1pos = new Vector2(1200, 500);
-            Spelare2pos = new Vector2(550, 480);
-
-
-
+            s1.Initialize();
+            s2.Initialize();
+            F.Initialize();
+            
             graphics.PreferredBackBufferWidth = GraphicsDevice.DisplayMode.Width;
             graphics.PreferredBackBufferHeight = GraphicsDevice.DisplayMode.Height;
             graphics.ApplyChanges();
 
-
-
-
-
-
-
             base.Initialize();
         }
-
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            Spelare1 = Content.Load<Texture2D>("Spelare1");
-            skott1 = Content.Load<Texture2D>("skott1");
-            skott2 = Content.Load<Texture2D>("skott2");
-            Spelare2 = Content.Load<Texture2D>("Spelare2");
+            s1.LoadContent(Content);
+            s2.LoadContent(Content);
+            F.LoadContent(Content);
             background = Content.Load<Texture2D>("background");
-            EnemySpawn = Content.Load<Texture2D>("EnemySpawn");
-
-
-            screenwidth = GraphicsDevice.Viewport.Width;
-            screenheight = GraphicsDevice.Viewport.Height;
-
-
-
-
-            // TODO: use this.Content to load your game content here
+   
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
+
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (kNewstate.IsKeyDown(Keys.Escape))
-                Exit();//om man trycker på escape så avslutas gamet
-
-
-
-            storlekspelare2 = new Rectangle((int)Spelare2pos.X, (int)Spelare2pos.Y, 150, 150);//storlek på fienden
-            storlekSpelare1 = new Rectangle((int)spelare1pos.X, (int)spelare1pos.Y, 130, 130);//storleken på spritebatchen Spelare1 
-
-
-            kNewstate = Keyboard.GetState();
+            if (quit == true)
+            {
+                Exit();
+            }
+            
             KeyboardState a = Keyboard.GetState();
 
-            if (a.IsKeyDown(Keys.Right))//Rör på Spelare1 sprite höger, vänster, upp, ned
-                spelare1pos.X += 10;
-            if (a.IsKeyDown(Keys.Left))
-                spelare1pos.X -= 10;
-            if (a.IsKeyDown(Keys.Up))
-                spelare1pos.Y -= 10;
-            if (a.IsKeyDown(Keys.Down))
-                spelare1pos.Y += 10;
-
-
-
-            if (a.IsKeyDown(Keys.Space) && kOldstate.IsKeyUp(Keys.Space))//skott för Spelare1 spriten
-            {
-                Spelare1skottpos.Add(spelare1pos + new Vector2(10, 0));
-            }
-            for (int i = 0; i < Spelare1skottpos.Count; i++)
-            {
-                Spelare1skottpos[i] = Spelare1skottpos[i] - new Vector2(0, 5);
-            }
-
-
-
-            if (a.IsKeyDown(Keys.D))// rörelse för fienden
-                Spelare2pos.X += 10;
-            if (a.IsKeyDown(Keys.A))
-                Spelare2pos.X -= 10;
-            if (a.IsKeyDown(Keys.W))
-                Spelare2pos.Y -= 10;
-            if (a.IsKeyDown(Keys.S))
-                Spelare2pos.Y += 10;
-
-
-
-
-            if (a.IsKeyDown(Keys.Z) && kOldstate.IsKeyUp(Keys.Z))//skott för fienden
-            {
-                Spelare2skottpos.Add(Spelare2pos + new Vector2(89, 0));
-            }
-            for (int i = 0; i < Spelare2skottpos.Count; i++)
-            {
-                Spelare2skottpos[i] = Spelare2skottpos[i] - new Vector2(0, 5);
-            }
-
-
-
-
-
-            if (spelare1pos.X <= 0)//gör så att Spelare1 spriten inte kan komma utanför skärmen i X-led
-                spelare1pos.X = 0;
-            if (spelare1pos.X + storlekSpelare1.Width >= screenwidth)
-                spelare1pos.X = screenwidth - storlekSpelare1.Width;
-            if (spelare1pos.Y <= 0) //i Y-led så att Spelare1 spriten inte kommer utanför skärmen
-                spelare1pos.Y = 0;
-            if (spelare1pos.Y + storlekSpelare1.Height >= screenheight)
-                spelare1pos.Y = screenheight - storlekSpelare1.Height;
-            if (Spelare2pos.X <= 0)//göt så att Spelare2 spriten inte kan komma utanför skärmen i X-led
-                Spelare2pos.X = 0;
-            if (Spelare2pos.X + storlekspelare2.Width >= screenwidth)
-                Spelare2pos.X = screenwidth - storlekspelare2.Width;
-            if (Spelare2pos.Y <= 0) //i Y-led så att Spelare2 spriten inte kommer utanför skärmen
-                Spelare2pos.Y = 0;
-            if (Spelare2pos.Y + storlekspelare2.Height >= screenheight)
-                Spelare2pos.Y = screenheight - storlekspelare2.Height;
-
-
-
-            if (SpawnRate > 15)//Om Spawnrate är över 15 så kommer den minska med 5 var 10 sekund, 10s = enemytimer 600
-            {
-                EnemyTimer++;
-                if (EnemyTimer == 120)
-                {
-                    SpawnRate -= 5;
-                    EnemyTimer = 0;
-                }
-            }
-            if (SpawnRate <= 15 && SpawnRate > 5)//Om Spawnrate är 5<x<15 så kommer den minska med 1 var 10 sekund, 10s = enemytimer 600
-            {
-                EnemyTimer++;
-                if (EnemyTimer == 120)
-                {
-                    SpawnRate -= 1;
-                    EnemyTimer = 0;
-                }
-            }
-            EnemySpawnPos = rnd.Next(0, screenwidth);
-            if (rnd.Next(0, SpawnRate) == 0)
-            {
-                RandomEnemySpawn.Add(new Vector2(EnemySpawnPos, 0));
-            }
-            for (int i = 0; i < RandomEnemySpawn.Count; i++)
-            {
-                RandomEnemySpawn[i] = RandomEnemySpawn[i] - new Vector2(0, -2);
-            }
-
-            RemoveObjects();
-
-            kOldstate = kNewstate;
-
-            // TODO: Add your update logic here
+            // om man trycker på escape så avslutas gamet
+            if (a.IsKeyDown(Keys.Escape))
+                Exit();
+        
+            s1.Update(gameTime);
+            s2.Update(gameTime);
+            F.Update(gameTime);
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-
             spriteBatch.Begin();
             spriteBatch.Draw(background, new Rectangle(0, 0, GraphicsDevice.DisplayMode.Width, GraphicsDevice.DisplayMode.Height), Color.White);
-            spriteBatch.Draw(Spelare1, storlekSpelare1, Color.White);
-            spriteBatch.Draw(Spelare2, storlekspelare2, Color.White);
-            spriteBatch.Draw(EnemySpawn, new Rectangle(0, 0, 0, 0), Color.White);
-
-
-
-
-            foreach (Vector2 Spelare1skottpos in Spelare1skottpos) //skott för spelare1
-            {
-                Rectangle rec = new Rectangle();
-                rec.Location = Spelare1skottpos.ToPoint();
-                rec.Size = new Point(30, 30);
-                spriteBatch.Draw(skott1, rec, Color.White);
-            }
-
-            foreach (Vector2 Spelare2skottpos in Spelare2skottpos)//skott för spelare2
-            {
-                Rectangle rec = new Rectangle();
-                rec.Location = Spelare2skottpos.ToPoint();
-                rec.Size = new Point(40, 40);
-                spriteBatch.Draw(skott2, rec, Color.White);
-            }
-
-
-            foreach (Vector2 RandomEnemySpawn in RandomEnemySpawn)
-            {
-                Rectangle rec = new Rectangle();
-                rec.Location = RandomEnemySpawn.ToPoint();
-                rec.Size = new Point(80, 80);
-                spriteBatch.Draw(EnemySpawn, rec, Color.White);
-            }
-
-
-
-
+            s1.Draw(spriteBatch);
+            s2.Draw(spriteBatch);
+            F.Draw(spriteBatch);
+           
             spriteBatch.End();
-
-
-
-            // TODO: Add your drawing code here
 
             base.Draw(gameTime);
         }
-        void RemoveObjects()
+
+        public static void Quit()
         {
-            List<Vector2> temp = new List<Vector2>();
-            foreach (var item in Spelare1skottpos)
-            {
-                if (item.Y >= 0)
-                {
-                    temp.Add(item);
-                }
-            }
-
-            Spelare1skottpos = temp;
-
-            List<Vector2> temp2 = new List<Vector2>();
-            foreach (var item in Spelare2skottpos)
-            {
-                if (item.Y >= 0)
-                {
-                    temp2.Add(item);
-                }
-            }
-
-            Spelare2skottpos = temp2;
-
-            List<Vector2> temp3 = new List<Vector2>();
-            foreach (var item in RandomEnemySpawn)
-            {
-                if (item.Y <= screenheight)
-                {
-                    temp3.Add(item);
-                }
-            }
-
-            RandomEnemySpawn = temp3;
+            quit = true;
         }
     }
 }
---
+++ Template/Spelare1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Template
+{
+    class Spelare1
+    {
+        private Texture2D Spelare2, skott2;
+        private static Vector2 Spelare2pos;
+        private Rectangle storlekSpelare2;
+
+        public Rectangle StorlekSpelare2
+        {
+            get { return storlekSpelare2; }
+            set { storlekSpelare2 = value; }
+        }
+
+        private List<Vector2> Spelare2skottpos = new List<Vector2>();
+
+        private KeyboardState kNewstate, kOldstate;
+
+        public Spelare1()//Konstruktor
+        {
+
+        }
+        public void Initialize()
+        {
+            Spelare2pos = new Vector2(550, 480);
+        }
+
+        public void LoadContent(ContentManager Content)//load content
+        {
+            Spelare2 = Content.Load<Texture2D>("Spelare2");
+            skott2 = Content.Load<Texture2D>("skott2");
+        }
+
+        public void Update(GameTime gameTime)//Update
+        {
+            kNewstate = Keyboard.GetState();
+
+            KeyboardState a = Keyboard.GetState();
+
+            //storlek på fienden
+            storlekSpelare2 = new Rectangle((int)Spelare2pos.X, (int)Spelare2pos.Y, 150, 150);
+
+            // rörelse för fienden
+            if (a.IsKeyDown(Keys.D))
+                Spelare2pos.X += 10;
+            if (a.IsKeyDown(Keys.A))
+                Spelare2pos.X -= 10;
+            if (a.IsKeyDown(Keys.W))
+                Spelare2pos.Y -= 10;
+            if (a.IsKeyDown(Keys.S))
+                Spelare2pos.Y += 10;
+
+            //skott för fienden
+            if (a.IsKeyDown(Keys.Z) && kOldstate.IsKeyUp(Keys.Z))
+            {
+                Spelare2skottpos.Add(Spelare2pos + new Vector2(89, 0));
+            }
+            for (int i = 0; i < Spelare2skottpos.Count; i++)
+            {
+                Spelare2skottpos[i] = Spelare2skottpos[i] - new Vector2(0, 5);
+            }
+
+            //Gör så att spelaren inte kan komma utanför skärmen
+            if (Spelare2pos.X <= 0)
+                Spelare2pos.X = 0;
+            if (Spelare2pos.X >= 1800)
+                Spelare2pos.X = 1800;
+            if (Spelare2pos.Y <= 0)
+                Spelare2pos.Y = 0;
+            if (Spelare2pos.Y >= 930)
+                Spelare2pos.Y = 930;
+        }
+        public void Draw(SpriteBatch spriteBatch)//Draw
+        {
+            spriteBatch.Draw(Spelare2, storlekSpelare2, Color.White);
+
+            //skott för spelare2
+            foreach (Vector2 Spelareskottpos in Spelare2skottpos)
+            {
+                Rectangle rec = new Rectangle();
+                rec.Location = Spelareskottpos.ToPoint();
+                rec.Size = new Point(40, 40);
+                spriteBatch.Draw(skott2, rec, Color.White);
+            }
+        }
+    }
+}
