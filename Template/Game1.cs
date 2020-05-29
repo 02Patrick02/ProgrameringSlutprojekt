f0f24c7eecd41dd588898a6ef53e9ec7142ee84d
02patrick02 <paku02001@utb.vaxjo.se>
2020-05-29 11:42:30 +0200

    Har gjort klart det mesta

--- Template/Game1.cs
+++ Template/Game1.cs
@@ -8,55 +8,62 @@ namespace Template
 {
     public class Game1 : Game
     {
-   
-        Spelare s1 = new Spelare();
-        Spelare1 s2 = new Spelare1();
-   
+        //refererar till min spelareklass
+        Spelare1 S;
+
+        //graphics ändrar hur stor skärmen ska vara
         private GraphicsDeviceManager graphics;
+
         private SpriteBatch spriteBatch;
 
+        //Random
         private Random rnd = new Random();
 
+        // Int är olika värden på heltal
         private int EnemySpawnPos = 0, EnemyTimer = 0, SpawnRate = 60, PowerTimer = 0, Spawnratepower = 1000, PowerPos = 0;
 
-
+        //sprites som ska ritas ut
         private Texture2D background, EnemySpawn, Power;
 
-        private Vector2 spelare1pos, Spelare2pos;
+        //positionen för spelaren
+        private Vector2 SpelarePos;
 
+        // listor
         private List<Vector2> RandomEnemySpawn = new List<Vector2>();
         private List<Vector2> RandomPowerSpawn = new List<Vector2>();
-        
+        private List<Vector2> SpelareSkottPos = new List<Vector2>();
+
+        //konstruktor
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            S = new Spelare1(SpelareSkottPos);
         }
 
         protected override void Initialize()
         {
-            s1.Initialize();
-            s2.Initialize();
-        
+            S.Initialize();
             
+            //Ändrar storleken på skärmen
             graphics.PreferredBackBufferWidth = GraphicsDevice.DisplayMode.Width;
             graphics.PreferredBackBufferHeight = GraphicsDevice.DisplayMode.Height;
             graphics.ApplyChanges();
 
             base.Initialize();
         }
+
+        //här skriver man allt som ska laddas in på skärmen"alla sprites"
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            s1.LoadContent(Content);
-            s2.LoadContent(Content);
+            S.LoadContent(Content);
+
             EnemySpawn = Content.Load<Texture2D>("EnemySpawn");
             background = Content.Load<Texture2D>("background");
             Power = Content.Load<Texture2D>("Power");
-            
-
-   
+             
         }
 
         protected override void UnloadContent()
@@ -66,14 +73,14 @@ namespace Template
 
         protected override void Update(GameTime gameTime)
         {
-            KeyboardState a = Keyboard.GetState();
+            KeyboardState a = Keyboard.GetState(); //istället för att skriva keyboardstate så kan man skriva a med samma funktion 
 
             // om man trycker på escape så avslutas monogame
             if (a.IsKeyDown(Keys.Escape))
                 Exit();
         
-            s1.Update(gameTime);
-            s2.Update(gameTime);
+            //S. refererar till min klass som heter Spelare1
+            S.Update(gameTime);
       
 
 
@@ -97,35 +104,27 @@ namespace Template
                     EnemyTimer = 0;
                 }
             }
+
+            //vart enemy ska spawna random mellan 0, 1800
             EnemySpawnPos = rnd.Next(0, 1800);
             if (rnd.Next(0, SpawnRate) == 0)
             {
                 RandomEnemySpawn.Add(new Vector2(EnemySpawnPos, 0));
             }
 
+           
+            //collision mellan enemy och spelaren då spelet stängs av om spelaren kolliderar
             for (int i = 0; i < RandomEnemySpawn.Count; i++)
             {
                 RandomEnemySpawn[i] = RandomEnemySpawn[i] - new Vector2(0, -2);
                 Rectangle rec = new Rectangle((int)RandomEnemySpawn[i].X, (int)RandomEnemySpawn[i].Y, 30, 50);
-                Rectangle storlekSpelare1 = new Rectangle((int)spelare1pos.X, (int)spelare1pos.Y, 130, 130);
-                if (s1.StorlekSpelare1.Intersects(rec))
+                Rectangle StorlekSpelare = new Rectangle((int)SpelarePos.X, (int)SpelarePos.Y, 130, 130);
+                if (S.Storlekspelare.Intersects(rec))
                 {
-                    Exit();
+                    RandomEnemySpawn.Remove(RandomEnemySpawn[i]);
                 }
             }
-
-
-            for (int i = 0; i < RandomEnemySpawn.Count; i++)
-            {
-                RandomEnemySpawn[i] = RandomEnemySpawn[i] - new Vector2(0, -2);
-                Rectangle rec = new Rectangle((int)RandomEnemySpawn[i].X, (int)RandomEnemySpawn[i].Y, 30, 50);
-                Rectangle storlekSpelare2 = new Rectangle((int)Spelare2pos.X, (int)Spelare2pos.Y, 130, 130);
-                if (s2.StorlekSpelare2.Intersects(rec))
-                {
-                    Exit();
-                }
-            }
-
+            // Spawn raten på powerupsen som dyker upp random på skärmen och de sapwnar fler ju längre man spelar
             if(Spawnratepower > 15)
             {
                 PowerTimer++;
@@ -151,16 +150,48 @@ namespace Template
                 RandomPowerSpawn.Add(new Vector2 (rnd.Next(0, 1900), rnd.Next(0, 950)));
             }
 
+            //kollision mellan skotten och enemy då båda försvinner från skärmen vid kollision
+            for (int i = 0; i < RandomEnemySpawn.Count; i++)
+            {
+                RandomEnemySpawn[i] = RandomEnemySpawn[i] - new Vector2(0, -2);
+                Rectangle rec = new Rectangle((int)RandomEnemySpawn[i].X, (int)RandomEnemySpawn[i].Y, 30, 50);
+                for(int j = 0; j < SpelareSkottPos.Count; j++)
+                {
+                    Rectangle Storlekskott = new Rectangle((int)SpelareSkottPos[j].X, (int)SpelareSkottPos[j].Y, 100, 100);
+
+                    if (Storlekskott.Intersects(rec))
+                    {
+                        SpelareSkottPos.Remove(SpelareSkottPos[j]);
+                        RandomEnemySpawn.Remove(RandomEnemySpawn[Math.Max(i,0)]);
+                        i--;
+                    }
+                }
+            }
+
+            //kollision mellan spelare och powerupsen, då spelaren kolliderar så får han + 2 speed permanent
+            for (int i = 0; i < RandomPowerSpawn.Count; i++)
+            {
+                Rectangle rec = new Rectangle((int)RandomPowerSpawn[i].X, (int)RandomPowerSpawn[i].Y, 10, 50);
+                Rectangle StorlekSpelare = new Rectangle((int)SpelarePos.X, (int)SpelarePos.Y, 130, 130);
+                if (S.Storlekspelare.Intersects(rec))
+                {
+                    RandomPowerSpawn.Remove(RandomPowerSpawn[i]);
+
+                    S.Speed += 2; // för varje gång spelaren tar upp poweruppen så ökar hastigheten permanent för spelaren.
+                }
+            }
+
             base.Update(gameTime);
         }
 
+        //ritar ut sprites
         protected override void Draw(GameTime gameTime)
         {
             spriteBatch.Begin();
+
             spriteBatch.Draw(background, new Rectangle(0, 0, GraphicsDevice.DisplayMode.Width, GraphicsDevice.DisplayMode.Height), Color.White);
             spriteBatch.Draw(Power, new Rectangle(GraphicsDevice.DisplayMode.Width, GraphicsDevice.DisplayMode.Height, 0, 0), Color.White);
-            s1.Draw(spriteBatch);
-            s2.Draw(spriteBatch);
+            S.Draw(spriteBatch);
            
             foreach (Vector2 RandomEnemySpawn in RandomEnemySpawn)
             {
@@ -177,10 +208,10 @@ namespace Template
                 spriteBatch.Draw(Power, rec, Color.White);
             }
 
-
             base.Draw(gameTime);
 
             spriteBatch.End();
+
         }
     }
 }
--- Template/Spelare1.cs
+++ Template/Spelare1.cs
@@ -10,88 +10,125 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Template
 {
-    class Spelare1
+    class Spelare1 // En class som heter Spelare1
     {
-        private Texture2D Spelare2, skott2;
-        private static Vector2 Spelare2pos;
-        private Rectangle storlekSpelare2;
+        //texture2D är alla sprite bilder som kommer vara synliga på skärmen
+        private Texture2D Spelare, Skott;
 
-        public Rectangle StorlekSpelare2
+        //Vector2 är positionen för sprites
+        private Vector2 SpelarePos, SkottPos;
+
+        //Rectangle är storleken på sprites
+        private Rectangle StorlekSpelare, StorlekSkott;
+
+        // en int variabel som ändrar på hastigheten
+        private int speed;
+
+        // En Get och Set metod för storlekspelare
+        public Rectangle Storlekspelare 
         {
-            get { return storlekSpelare2; }
-            set { storlekSpelare2 = value; }
+            get { return StorlekSpelare; }
+            set { StorlekSpelare = value; }
         }
 
-        private List<Vector2> Spelare2skottpos = new List<Vector2>();
+        // En Get och Set metod för storlekskott
+        public Rectangle Storlekskott
+        {
+            get { return StorlekSkott; }
+            set { StorlekSkott = value; }
+        }
 
+        // En Get och Set metod för speed
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        // En Lista flr Skotten till spelaren
+        private List<Vector2> SpelareSkottPos = new List<Vector2>();
+
+        // tillåter att kNewstate och k0ldstate får användas
         private KeyboardState kNewstate, kOldstate;
 
-        public Spelare1()//Konstruktor
+        //Konstruktor
+        public Spelare1(List<Vector2>skottLista)
         {
-
+            SpelareSkottPos = skottLista;
         }
+
+        //I initialize så skriver man in det som ska ske när spelet börjar
         public void Initialize()
         {
-            Spelare2pos = new Vector2(550, 480);
+            SpelarePos = new Vector2(550, 480);
+            SkottPos = new Vector2(550, 480);
+            Speed = 10;
         }
 
-        public void LoadContent(ContentManager Content)//load content
+        //load content
+        public void LoadContent(ContentManager Content)
         {
-            Spelare2 = Content.Load<Texture2D>("Spelare2");
-            skott2 = Content.Load<Texture2D>("skott2");
+            Spelare = Content.Load<Texture2D>("Spelare");
+            Skott = Content.Load<Texture2D>("Skott");
         }
 
-        public void Update(GameTime gameTime)//Update
+        //Update
+        public void Update(GameTime gameTime)
         {
-            kNewstate = Keyboard.GetState();
-
+         
             KeyboardState a = Keyboard.GetState();
 
-            //storlek på fienden
-            storlekSpelare2 = new Rectangle((int)Spelare2pos.X, (int)Spelare2pos.Y, 150, 150);
+            //storlek på Spelare
+            StorlekSpelare = new Rectangle((int)SpelarePos.X, (int)SpelarePos.Y, 150, 150);
+            //Skottens storlek
+            StorlekSkott = new Rectangle((int)SkottPos.X, (int)SkottPos.Y, 150, 150);
+       
 
-            // rörelse för fienden
+            // rörelse för spelare
             if (a.IsKeyDown(Keys.D))
-                Spelare2pos.X += 10;
+                SpelarePos.X += speed;
             if (a.IsKeyDown(Keys.A))
-                Spelare2pos.X -= 10;
+                SpelarePos.X -= speed;
             if (a.IsKeyDown(Keys.W))
-                Spelare2pos.Y -= 10;
+                SpelarePos.Y -= speed;
             if (a.IsKeyDown(Keys.S))
-                Spelare2pos.Y += 10;
+                SpelarePos.Y += speed;
 
-            //skott för fienden
-            if (a.IsKeyDown(Keys.Z) && kOldstate.IsKeyUp(Keys.Z))
+            //Skott för spelare
+            if (a.IsKeyDown(Keys.Space) && kOldstate.IsKeyUp(Keys.Space))
             {
-                Spelare2skottpos.Add(Spelare2pos + new Vector2(89, 0));
+                SpelareSkottPos.Add(SpelarePos + new Vector2(89, 0));
             }
-            for (int i = 0; i < Spelare2skottpos.Count; i++)
+            for (int i = 0; i < SpelareSkottPos.Count; i++)
             {
-                Spelare2skottpos[i] = Spelare2skottpos[i] - new Vector2(0, 5);
+                SpelareSkottPos[i] = SpelareSkottPos[i] - new Vector2(0, 5);
             }
 
 
             //Gör så att spelaren inte kan komma utanför skärmen
-            if (Spelare2pos.X <= 0)
-                Spelare2pos.X = 0;
-            if (Spelare2pos.X >= 1800)
-                Spelare2pos.X = 1800;
-            if (Spelare2pos.Y <= 0)
-                Spelare2pos.Y = 0;
-            if (Spelare2pos.Y >= 930)
-                Spelare2pos.Y = 930;
+            if (SpelarePos.X <= 0)
+                SpelarePos.X = 0;
+            if (SpelarePos.X >= 1800)
+                SpelarePos.X = 1800;
+            if (SpelarePos.Y <= 0)
+                SpelarePos.Y = 0;
+            if (SpelarePos.Y >= 930)
+                SpelarePos.Y = 930;
         }
-        public void Draw(SpriteBatch spriteBatch)//Draw
-        {
-            spriteBatch.Draw(Spelare2, storlekSpelare2, Color.White);
+ 
 
-            //skott för spelare2
-            foreach (Vector2 Spelareskottpos in Spelare2skottpos)
+        //Draw metoden som ritar ut sprites
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Spelare, StorlekSpelare, Color.White);
+
+            //ändrar på Skotten från spelaren
+            foreach (Vector2 Spelareskottpos in SpelareSkottPos)
             {
                 Rectangle rec = new Rectangle();
                 rec.Location = Spelareskottpos.ToPoint();
                 rec.Size = new Point(40, 40);
-                spriteBatch.Draw(skott2, rec, Color.White);
+                spriteBatch.Draw(Skott, rec, Color.White);
             }
         }
     }
